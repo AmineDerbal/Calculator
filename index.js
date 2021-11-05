cb6f305e35a8ec458da32d0b837d29c856778cdc
Kyu931 <aminedoran31@gmail.com>
2021-11-05 16:48:35 +0100

    some progress

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const displayTop = document.querySelector("#display-top");
 const displayBottom = document.querySelector("#display-bottom");
 const btns = document.querySelectorAll(".calcualtor-body button");
 const dot = document.querySelector("#dot");
+const number = document.querySelectorAll(".calcualtor-body .number");
 
 let operator = "";
 let val1 = "";
@@ -10,7 +11,6 @@ let val2 = "";
 displayBottom.value = "";
 displayTop.value = "";
 dot.disabled = false;
-let negation = false;
 
 btns.forEach((button) => {
   button.addEventListener("click", function (e) {
@@ -19,7 +19,16 @@ btns.forEach((button) => {
 
     //case class is number
     if (className == "number") {
+      let lastChar = displayBottom.value.charAt(displayBottom.value.length - 1);
+
       if (displayBottom.value == "0" && displayBottom.value.length == 1) {
+        displayBottom.value = e.target.id;
+      } else if (
+        lastChar == "+" ||
+        lastChar == "x" ||
+        lastChar == "-" ||
+        lastChar == "÷"
+      ) {
         displayBottom.value = e.target.id;
       } else {
         displayBottom.value += e.target.id;
@@ -30,6 +39,9 @@ btns.forEach((button) => {
       displayBottom.value = "";
       displayTop.value = "";
       dot.disabled = false;
+      val1 = "";
+      val2 = "";
+      operator = "";
     }
 
     // case class is delete
@@ -59,12 +71,10 @@ btns.forEach((button) => {
 
     //case +/-
     else if (className == "+/-") {
-      if (negation == false) {
+      if (displayBottom.value.charAt(0) != "-") {
         displayBottom.value = "-" + displayBottom.value;
-        negation = true;
-      } else if (negation == true && displayBottom.value.charAt(0) == "-") {
+      } else if (displayBottom.value.charAt(0) == "-") {
         displayBottom.value = displayBottom.value.slice(1);
-        negation = false;
       }
     }
 
@@ -73,22 +83,93 @@ btns.forEach((button) => {
       operation(id);
     }
 
-    // coding the buttons function
+    //case =
+    else if (className == "=") {
+      if (displayBottom.value != "" && operator != "" && val1 !== "") {
+        val2 = displayBottom.value;
+        equal();
+      }
+    }
   });
 });
 
+// case operator + x - ÷
 function operation(id) {
   if (val1 == "" && displayBottom.value != "") {
     operator = id;
     val1 = displayBottom.value;
     displayTop.value = val1 + " " + operator;
     dot.disabled = false;
-    negation = false;
+
     displayBottom.value = "";
   } else if (val1 !== "" && displayBottom.value != "") {
     val2 = displayBottom.value;
+    displayTop.value += " " + val2 + " " + id;
     equal();
+    operator = id;
+    displayBottom.value += operator;
   }
 }
 
-function equal() {}
+function equal() {
+  if (operator == "+") {
+    res = addition(val1, val2);
+    displayBottom.value = res;
+    val1 = res;
+    //displayTop.value = val1 + " " + operator + " " + val2;
+    emptyValue();
+
+    if (isInteger(res)) {
+      dot.disabled = false;
+    } else {
+      dot.disabled = true;
+    }
+  } else if (operator == "-") {
+    res = substraction(val1, val2);
+    displayBottom.value = res;
+    displayTop.value = val1 + " " + operator + " " + val2;
+    emptyValue();
+    if (isInteger(res)) {
+      dot.disabled = false;
+    } else {
+      dot.disabled = true;
+    }
+  } else if (operator == "x") {
+    res = multipication(val1, val2);
+    displayBottom.value = res;
+    displayTop.value = val1 + " " + operator + " " + val2;
+    emptyValue();
+
+    if (isInteger(res)) {
+      dot.disabled = false;
+    } else {
+      dot.disabled = true;
+    }
+  } else if (operator == "÷") {
+    if (val2 != "0") {
+      res = division(val1, val2);
+      displayBottom.value = res;
+      displayTop.value = val1 + " " + operator + " " + val2;
+      emptyValue();
+
+      if (isInteger(res)) {
+        dot.disabled = false;
+      } else {
+        dot.disabled = true;
+      }
+    } else {
+      alert("impossibe to divise a number by 0");
+      val2 = "";
+    }
+  }
+}
+
+addition = (val1, val2) => parseFloat(val1) + parseFloat(val2);
+substraction = (val1, val2) => parseFloat(val1) - parseFloat(val2);
+multipication = (val1, val2) => parseFloat(val1) * parseFloat(val2);
+division = (val1, val2) => parseFloat(val1) / parseFloat(val2);
+isInteger = (res) => (res % 1 == 0 ? true : false);
+emptyValue = () => {
+  val2 = "";
+  operator = "";
+};
